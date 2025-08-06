8754e29b6cb6928d9ef2699776d16752a7b80a3d
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-08-05 21:24:15 -0500

    Dependencies tutorial

--- custom_log.py
+++ custom_log.py
@@ -1,4 +1,7 @@
-def log(tag="", message=""):
+from fastapi import Request
+
+
+def log(tag="MyApp", message="", request: Request = None):
     """
     Custom logging function to print messages with a tag.
 
@@ -7,5 +10,9 @@ def log(tag="", message=""):
         message (str): The message to log.
     """
     print(f"Logging: [{tag}] {message}")
-    with open("log.txt", "w+") as log:
+    with open("log.txt", "a+") as log:
         log.write(f"{tag}: {message}\n")
+        if request:
+            log.write(f"\t{request.method} {request.url}\n")
+            log.write(f"\tHeaders: {request.headers}\n")
+            log.write(f"\tQuery Params: {request.query_params}\n")
--- routers/dependency.py
+++ routers/dependency.py
@@ -1,16 +1,41 @@
 from fastapi import APIRouter, Depends
 from fastapi.requests import Request
+from custom_log import log
 
-router = APIRouter(prefix="/dependency", tags=["Dependency Injection"])
+router = APIRouter(
+    prefix="/dependency", tags=["Dependency Injection"], dependencies=[Depends(log)]
+)
 
 
-def convert_headers(request: Request):
+def convert_params(request: Request, seperator: str):
+    query = []
+    for key, value in request.query_params.items():
+        query.append(f"{key} {seperator} {value}")
+    return query
+
+
+def convert_headers(
+    request: Request, seperator: str = "==", query=Depends(convert_params)
+):
     out_headers = []
     for key, value in request.headers.items():
-        out_headers.append(f"{key} -- {value}")
-    return out_headers
+        out_headers.append(f"{key} {seperator} {value}")
+    return {"headers": out_headers, "query": query}
 
 
 @router.get("")
 def get_items(headers=Depends(convert_headers)):
     return {"headers": headers}
+
+
+class Account:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+
+@router.post("/user")
+def create_user(
+    name: str, email: str, password: str, account: Account = Depends(Account)
+):
+    return {"name": account.name, "email": account.email, "password": password}
